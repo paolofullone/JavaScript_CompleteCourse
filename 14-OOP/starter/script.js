d09055ec062eb9944acb9d41506537b73b6ca045
paolofullone <paolo.fullone@gmail.com>
2021-10-03 19:56:30 -0300

    Car coding challenge.

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //? 203 Constructor Functions and the new Operator.mp4
-
+/*
 // In OOP there's a convention that Constructor Functions always starts with a capital letter. Arrow function doest not work with
 // Constructor Functions because it does not have the this keyword. Only function declarations and function expressions works.
 const Person = function (firstName, birthYear) {
@@ -137,3 +137,54 @@ console.dir(h1); // as we can see the Prototype in the end of the object is a HT
 
 // Any function is also an object and therefore it also has prototype.
 console.dir(x => x + 1); // This anonymous function contains apply, bind and call...That's why we can call these functions on it...
+*/
+//? 207 Coding Challenge #1.mp4
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} Km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} Km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+console.log(bmw);
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
+bmw.accelerate();
+
+const mercedes = new Car('Mercedes', 95);
+console.log(mercedes);
+mercedes.accelerate();
+mercedes.brake();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
