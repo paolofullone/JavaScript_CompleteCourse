cacfe4798d20d5105c040ff18c95dba739bb9733
paolofullone <paolo.fullone@gmail.com>
2021-10-13 15:18:23 -0300

    246 - API Country - render data

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,53 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+// //? 242 Our First AJAX Call_ XMLHttpRequest.mp4
+
+// We have multiple ways of doing AJAX requests in JS, we will start with the most old school one.
+
+// visit github public apis => https://github.com/public-apis/public-apis
+// CORS stands for cross origin resource sharing, we should look for the ones with YES or Unknown. Without CORS we cannot access a
+// 3rd party api from our code.
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+  // with the command above we will make the request to the chosen API, then we will listen the completion of the request and the arrival of
+  // the data with an event listener on request with 'load' parameter.
+
+  request.addEventListener('load', function () {
+    //   console.log(this.responseText); // the this keyword here is request.
+    //   console.log(request.responseText); // the this keyword here is request.
+
+    // now let's convert the data to a JS object using parse again.
+    //   const data = JSON.parse(this.responseText); // we can destructure in two ways:
+    //   const data = JSON.parse(this.responseText)[0]; // 1
+    const [data] = JSON.parse(this.responseText); // 2 - More beautiful and we get our object directly.
+    console.log(data);
+
+    const html = `
+      <article class="country">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+              <h3 class="country__name">${data.name}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${
+                // + converts to number, / by 1 million and rounded to 1 decimal case.
+                (+data.population / 1_000_000).toFixed(2)
+              }</p>
+              <p class="country__row"><span>🗣️</span>${
+                data.languages[0].name
+              }</p>
+              <p class="country__row"><span>💰</span>${
+                data.currencies[0].name
+              }</p>
+            </div>
+      </article>
+          `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountryData('portugal');
+
+//? 247 - Sequence of AJAX calls, we will render the border country of Portugal (Spain)
---
+++ 16-Asynchronous/starter/script_v3.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const REST_COUNTRIES = 'https://restcountries.com/v3/name';
+
+function getCountryData(countryName) {
+  const request = new XMLHttpRequest();
+
+  // console.log('open...');
+  request.open('GET', `${REST_COUNTRIES}/${countryName}`);
+
+  // console.log('send...');
+  request.send();
+
+  // console.log('waiting for response...');
+  request.addEventListener('load', function () {
+    // console.log('got a response!');
+    const country = JSON.parse(this.responseText)[0];
+    console.log(country);
+
+    const currency_key = Object.keys(country.currencies)[0];
+    const currency = country.currencies[currency_key].name;
+
+    const language_key = Object.keys(country.languages)[0];
+    const language = country.languages[language_key];
+
+    const population = `${(country.population / 1_000_000).toFixed(2)} M`;
+
+    const html = `
+      <article class="country">
+      <img class="country__img" src="${country.flags[0]}"/>
+      <div class="country__data">
+        <h3 class="country__name">${country.name.common}</h3>
+        <h4 class="country__region">${country.region}</h4>
+        <p class="country__row"><span>👫</span>${population}</p>
+        <p class="country__row"><span>🗣️</span>${language}</p>
+        <p class="country__row"><span>💰</span>${currency}</p>
+      </div>
+    </article>
+      `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+}
+
+getCountryData('brazil');
+getCountryData('estonia');
+getCountryData('lebanon');
