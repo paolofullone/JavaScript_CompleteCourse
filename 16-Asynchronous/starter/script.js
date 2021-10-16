a326053cde546e30665cc5731f93d88f738a8822
paolofullone <paolo.fullone@gmail.com>
2021-10-16 08:43:33 -0300

    256-partial

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
@@ -12,6 +12,7 @@ const countriesContainer = document.querySelector('.countries');
 // CORS stands for cross origin resource sharing, we should look for the ones with YES or Unknown. Without CORS we cannot access a
 // 3rd party api from our code.
 */
+/*
 const renderCountry = function (data, className = '') {
   const html = `
       <article class="country ${className}">
@@ -33,12 +34,12 @@ const renderCountry = function (data, className = '') {
       </article>
           `;
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  // countriesContainer.style.opacity = 1; // removed and included in the .finally method.
+  countriesContainer.style.opacity = 1; // removed and included in the .finally method.
 };
 
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
-  // countriesContainer.style.opacity = 1; // removed and included in the .finally method.
+  countriesContainer.style.opacity = 1; // removed and included in the .finally method.
 };
 
 /*
@@ -131,7 +132,7 @@ getCountryAndNeighbour('usa');
 
 // refactoring removing the console.logs and transforming into arrow functions.
 // so we replaced the code written before with callbacks, event listeners etc by this small block of code.
-*/
+
 
 const getJSON = function (url, errorMsg = 'Something went wrong') {
   return fetch(url).then(response => {
@@ -204,7 +205,7 @@ btn.addEventListener('click', function () {
   getCountryData('australia');
 });
 
-getCountryData('brazil'); // this returns a "cannot read properties of undefined (reading 'flag')" but the error is that theres no
+// getCountryData('brazil'); // this returns a "cannot read properties of undefined (reading 'flag')" but the error is that theres no
 // scalabosh country, so we will see how to handle this error.
 
 // 249 - Chain promises to render the initial country and neighbour. chain 2 AJAX
@@ -222,3 +223,130 @@ getCountryData('brazil'); // this returns a "cannot read properties of undefined
 //* 251 - Handling rejected promises (errors in promises)
 
 //* 252 - Throwing errors manually
+
+//* 253
+
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. 
+Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. 
+Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you received about the provided location. Then, using this data, 
+log a message like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. 
+Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API 
+that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀 
+*/
+
+// const whereAmI = function (lat, lng) {
+//   fetch(
+//     `https://geocode.xyz/${lat},${lng}?geoit=json&auth=278829738579047305467x63888 `
+//   )
+//     .then(response => {
+//       if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+//       // console.log(response);
+//       return response.json();
+//     })
+//     .then(result => {
+//       // console.log(result);
+//       console.log(`You are in ${result.city}, ${result.country}.`);
+//       // const country = result.country;
+//       // return country;
+//     })
+//     .catch(err => {
+//       console.error(`${err} 💥💥💥`);
+//       renderError(`Something went wrong 💥💥💥  ${err.message}. Try again!`);
+//     });
+// };
+
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
+/*
+// Jonas Solution
+const whereAmI = function (lat, lng) {
+  fetch(
+    `https://geocode.xyz/${lat},${lng}?geoit=json&auth=278829738579047305467x63888 `
+  )
+    .then(res => {
+      // console.log(res);
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message} 💥`));
+};
+
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
+*/
+
+// 255 - Event Loop in practice
+
+// console.log('Test start');
+// setTimeout(() => console.log('0 sec timer'), 0);
+// Promise.resolve('Resolved promise 1').then(res => console.log(res));
+// console.log('Test end');
+
+// so both cl will be logged immediately because code outside any callback will run first.
+// Between the timer and the Promise it is trickier, they both finish at the exact same time. After 0 seconds. Because we used a .resolve
+// in promise and a 0 in timer.
+// The Promise will be handled by the micro-task queue, so it has priority over the callback queue.
+
+// 2nd test
+/*
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 100_000_000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');
+// This 2nd promise will delay the '0 sec timer' to show in the console. This proves that the 0 seconds we have as a parameter
+// of the SetTimeout is not a guarantee.
+//* We cannot do higher precision things using timers.
+*/
+
+//256 - Building a simple promise.
+
+// Lets say we
+const lotteryPromise = new Promise(function (resolve, reject) {
+  if (Math.random() >= 0.5) {
+    resolve('You win 💰');
+  } else {
+    reject('You lost your 💰');
+  }
+});
