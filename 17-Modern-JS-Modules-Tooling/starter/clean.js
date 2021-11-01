cfa0662e9aa10cf53ac255254e9419d9200cd532
paolofullone <paolo.fullone@gmail.com>
2021-11-01 08:38:38 -0300

    aula 277 - fix some bad code

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,4 @@
-var budget = [
+const budget = [
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -9,56 +9,87 @@ var budget = [
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
 ];
 
-var limits = {
+const spendingLimits = {
   jonas: 1500,
   matilda: 100,
 };
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
+// We had this in addExpense and CheckExpenses, so refactoring it:
+
+const getLimit = user => spendingLimits?.[user] ?? 0;
+
+const addExpense = function (value, description, user = 'jonas') {
+  // if (!user) user = 'jonas'; // if there's no user, set to jonas. same as default parameter.
   user = user.toLowerCase();
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
+  // let lim;
+  // // check if the user informed exists in spendingLimits and set the limit to the user, otherwise set to 0.
+  // if (spendingLimits[user]) {
+  //   lim = spendingLimits[user];
+  // } else {
+  //   lim = 0;
+  // }
 
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
-  }
-};
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
-console.log(budget);
+  // we can do it like this:
+  // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
+  // or use optional chaining:
+  // const limit = spendingLimits?.[user] ?? 0; // having only "spendingLimits?.[user]" means that if the spendingLimits of this user exists it will be set to the limit, otherwise it will be undefined, so we add the nulish covalescent operator to set it to zero.
 
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
+  // refactored to:
+  // const limit = getLimit(user);
+  // Refactored to getLimit and put it inside the if check below.
+
+  // if the value is less than the limit, the expense is added , otherwise its not added.
+  if (value <= getLimit(user)) {
+    // budget.push({ value: -value, description: description, user: user });
+    budget.push({ value: -value, description, user });
   }
 };
-check();
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
 
-console.log(budget);
+// const check = function () {
+const checkExpenses = function () {
+  // let lim;
+  // if (spendingLimits[entry.user]) {
+  //   lim = spendingLimits[entry.user];
+  // } else {
+  //   lim = 0;
+  // }
 
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
-  }
+  // Replaced by:
+  // const limit = spendingLimits?.[entry.user] ?? 0;
+  // Refactored to getLimit and put it inside the if check below.
+
+  for (const entry of budget)
+    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit'; //in the array we can se a limit flag on this the items above limit.
+};
+// With te refactored to we were able to remove two pairs of {}
+
+checkExpenses();
+
+const loBigExpenses = function (bigLimit) {
+  let output = '';
+  // if (entry.value <= -bigLimit) {
+  //   // output += entry.description.slice(-2) + ' / '; // Emojis are 2 chars
+  //   output += `${entry.description.slice(-2)}  / `; // Emojis counts as 2 chars
+  // }
+
+  // If replaced by ternary operator (also allowing to remove one pair of {})
+
+  for (const entry of budget)
+    output +=
+      entry.value <= -bigLimit ? `${entry.description.slice(-2)}  / ` : '';
+
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
+
+console.log(budget);
+loBigExpenses(500);
+
+// 1 - canged funtion name add to addExpense
+// 2 - changed all the VAR to CONST
+// 3 - changed limits to spendingLimits
--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,6 +1,5 @@
 //269 - Exporting and Importing in ES6 Modules.
 /*
-
 // Importing Module - since the imports are hoisted, we put them in the top of the file.
 // all the imports are executed first, even if we put this in the end of the file.
 
@@ -23,8 +22,11 @@ console.log('Importing Module');
 // Export default we don't define any name for it, we will define in the import, we use export default ...
 import add, { cart } from './shoppingCart.js';
 add('pizza', 2);
-add('bread', 2);
-add('apples', 2);
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart);
+/*
 
 //* We usually never mix named and default exports in the module. This works:
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
@@ -71,7 +73,7 @@ ShoppingCart2.addToCart('apple', 2);
 ShoppingCart2.addToCart('banana', 6);
 console.log(ShoppingCart2); // everything we returned.
 console.log(shoppingCart.shippingCost); // undefined because it is private.
-*/
+
 
 // 271 - commonJS modules
 
@@ -101,7 +103,10 @@ console.log(shoppingCart.shippingCost); // undefined because it is private.
 // npm i lodash-es => we can use install or i
 
 // now lets import clondeDeep from lodash-es
-import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+// with parcel it gets easier.
+import cloneDeep from 'lodash-es';
 
 const state = {
   cart: [
@@ -124,3 +129,105 @@ state.user.loggedIn = false; // now the stateClone is also loggedIn = false... t
 
 // using git we can ignore the node_modules files, because if we move it to another computer the we can simply
 // use "npm i"" in the terminal and it will download all the necessary files.
+
+//274 - Bundling with parcel and npm scripts
+
+// npm i parcel => to install
+
+// To use parcel we can use npx
+// npx parcel index.html // in terminal.
+
+// if something does not work use npm uninstal parcel then npm i parcel@1.12.4
+
+// module.hot means that if we change one of the modules it will trigger a rebuild and it will automatically (like magic) get injected in the browser without triggering a page reload. This will help in the maintanence of the page, because we won't need to login in again like we did in the bankist application.
+if (module.hot) {
+  module.hot.accept();
+}
+
+// we added "start": "parcel index.html" to package.json file at scripts section now we can run "npm run start" in terminal so we have the parcel running.
+
+// to build the final bundle of parcel we need a nother command. ""build": "parcel build index.html" then we do npm run build
+
+// 275 - Install Babel and Pollyfiling - makes everything compatible with windows 7, xp etc.
+
+// https://babeljs.io/ has official presets that transpile the code to older brownsers.
+
+// This didn't worked before with Babel and a plugin was necessary. Now its fine
+class Person {
+  greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.greeting}, ${this.name}`);
+  }
+}
+
+const jonas = new Person('Paolo');
+
+// Nulish covalescent operator
+console.log('Paolo Doido' ?? null);
+
+console.log(cart.find(el => (el.quantity = 2)));
+
+Promise.resolve('TEST').then(x => console.log(x));
+
+// Babel is able to transpile from ES6 to ES5, however *new* features to the language cannot be tanspiled becaust they didnt existed before. Babel can get an arrow function and convert to regular funcion. Bur will not change anything in a Promise. The new features needs to be polyfilled.
+
+// In the video the command below didn't worked, so run "npm install core-js"
+import 'core-js/stable';
+
+// Polyfill will create all the new functions like Array.filter, find, map... that were inserted in ES6 to work with old browsers.
+
+// For polyfilling async functions:
+// also worked automatically with 2.0, not needed => npm install renegerator runtime
+import 'regenerator-runtime/runtime';
+
+*/
+// 276. Review: Writing Clean and Modern JavaScript
+
+//* VERY GOOD REVIEW
+
+/*
+
+//* Readeble code
+=> code that OTHERS can understand
+=> code that YOU will understand in one year
+=> avoid code too CLEVER and OVERCOMPLICATED solution
+=> describe variables with WHAT THEY CONTAIN
+=> descriptive funciton names WHAT THEY DO
+
+//* GENERAL
+=> use DRY Principle and refactor your code 
+=> encasuplate your data info functions in order to not pollute global namespace
+=> don't use var, use let or const
+=> use strong type checks "===" or "!==" in order to do type checks.
+
+//* Functions:
+=> usually the functions should do ONE think, small and do it really well
+=> don't use more than 3 parameters (linked to previous ones)
+=> use default parameters whenever possible
+=> return the SAME data type as received
+=> use the arrow functions whenever they made the code more readable (in callback functions of array methods is a good use case for arrow functions)
+
+//* OOP
+=> use ES6 classes
+=> Encapsulate data that is not accessible from the outside, don't mutate outside the classes. When you need to manipulate the data, use a public API exactly the way you want it to happen.
+=> Method chaining makes the code easier to read and understand
+=> Don't use the arrow functions as methods (in regular objects), you will not get acces to the this keyword. EVEN IF YOU ARE NOT USING THE THIS KEYWORD, don't use the arrow functions. This will keep you from commit many mistakes.
+
+//* AVOID NESTED CODE
+=> Guard Clauses => early return in case some conditino is not achieved.
+=> Use ternary operator or logical operators instead of IF statements.
+=> If you really need the IF, use multiple IFs instead of if-else
+=> Avoid any kind of for and for of loops, use the array methods (map, filter and reduce) instead
+=> Avoid callback-based asynchronous APIs
+
+//* Asynchronous code:
+
+=> Consume promises with async/await instead of cath and then methods, because they need callback functions that will introduce more nested code. 
+=> Use promises and consume them with async/await instead. Whenever you can (if they can run in the same time and do not depend on each other), run promises in parallel with Promise.all. This will make the application faster.
+=> Always handle errors and promise rejecttions.
+
+*/
+
+//277 - Let's fix some bad code.
+// we will work in the clean.js file.
